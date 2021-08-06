2e1a9a987e5f1f7c3bd9643bf50e492b9f8eff79
Kyler Johnson <kjohnson@octolabs.io>
2021-08-06 17:30:59 -0400

    add libs

---
+++ libs/tour-of-heroes/hero-details/feature-hero-details/README.md
@@ -0,0 +1,7 @@
+# tour-of-heroes-hero-details-feature-hero-details
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test tour-of-heroes-hero-details-feature-hero-details` to execute the unit tests.
---
+++ libs/tour-of-heroes/hero-details/feature-hero-details/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'tour-of-heroes-hero-details-feature-hero-details',
+  preset: '../../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory:
+    '../../../../coverage/libs/tour-of-heroes/hero-details/feature-hero-details',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
